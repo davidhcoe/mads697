c39e5039bd9aa805876bcc1b048f672f1104b51d
David Coe <coedavid@umich.edu>
2022-08-03 18:35:07 -0400

    national average for some

--- county_details.py
+++ county_details.py
@@ -1,57 +1,97 @@
+from typing import Any, Dict
 import streamlit as st
 import pandas as pd 
 import altair as alt
+import queries
 
-url_params = st.experimental_get_query_params()
+@st.cache(suppress_st_warning=True)
+def get_dataframe():
+    df = pd.read_csv('counties_merged.csv')
 
-fips_parameter = 'fips'
+    averages = {}
 
-fips_code = ''
+    feature_names = ['population','white','black','native_american','asian','hawaiian','some_other_race_alone','two_more_races','hispanic_or_latino',
+                 'white_employed_16_64', 'black_employed_16_64',  'american_indian_employed_16_64', 'asian_employed_16_64', 'some_other_race_alone_employed_16_64',
+                 'two_or_more_race_employed_16_64', 'hispanic_or_latino_employed_16_64', 'employed_25_54_population', 'employed_16_64_population',
+                 'median_family_income','income_20_percentile','income_80_percentile','median_family_income_white','median_family_income_black',
+                 'median_family_income_indigenous','median_family_income_asian','median_family_income_hispanic',
+                 'some_other_race_under_5','two_or_more_race_under_5', 'preschool_enrollment_white','preschool_enrollment_black','preschool_enrollment_hispanic','preschool_enrollment_indigenous','preschool_enrollment_asian','year','public_students_pre_12','white_employed_16_64','black_employed_16_64','american_indian_employed_16_64','asian_employed_16_64','some_other_race_alone_employed_16_64','two_or_more_race_employed_16_64','hispanic_or_latino_employed_16_64','employed_25_54_population','employed_16_64_population','preschool_enroll','white_under_5','black_under_5','indigenous_under_5','asian_under_5','hispanic_under_5','avg_edu_prof_diff','low_birth_rate','Not Hispanic or Latino_low_birth_rate','Hispanic or Latino_low_birth_rate','Unknown or Not Stated_low_birth_rate','Black or African American_low_birth_rate','White_low_birth_rate','Asian_low_birth_rate','More than one race_low_birth_rate','American Indian or Alaska Native_low_birth_rate','Native Hawaiian or Other Pacific Islander_low_birth_rate','HPSA Score','HOM_STUDENTS','proportion_homeless','proportion_voter','proportion_high_poverty_neighborhood','transit_trips_index','transit_low_cost_index','crime_rate']
 
-if fips_parameter in url_params:
-    fips_code = url_params[fips_parameter][0]
+    for f in feature_names:
+        averages[f] = df[f].mean()
+
+    return df, averages
+
+def get_metric(name: str, col_name: str,county_df: pd.DataFrame, averages:Dict[str,Any], format_pattern:str=None, delta_color='normal'):
+     
+    value = county_df[col_name].values[0]
+
+    delta = None
+
+    if value:
+        value = float(value)
+        delta = round(value/averages[col_name], 1)
     
-    if len(fips_code) > 0:
-        df = pd.read_csv('counties_merged.csv')
+    if format_pattern is not None:
+        value = format_pattern.format(value)
 
-        county_only_df = df[df['FIPS']==int(fips_code)]
+    if delta is not None:
+        delta_str = f'{delta}% from national average'
 
-        if len(county_only_df) == 1:
-            st.title(f"Get data for {county_only_df.NAME.values[0]}")
+        return st.metric(name,
+            value = value, 
+            delta=delta_str, 
+            delta_color=delta_color
+        )
+    else:
+        return st.metric(name,
+            value = value
+        )
 
-            st.write(county_only_df)
+def build_page_content(fips_code: int):
 
-            st.write('Race')
+    df, averages = get_dataframe()
 
-            categories = ['white','black','native_american','asian','hawaiian',	'some_other_race_alone','two_more_races','hispanic_or_latino']
+    print(averages)
 
-            values = []
+    county_only_df = df[df['FIPS']==int(fips_code)]
 
-            value_texts = []
+    if len(county_only_df) == 1:
+        st.title(f"Get data for {county_only_df.NAME.values[0]}")
 
-            for c in categories:
-                value = county_only_df[c].values[0]
-                values.append(value)
-                value_texts.append('{:.0%}'.format(value))
+        st.write(county_only_df)
 
-            chart_df = pd.DataFrame({"race": categories, "value": values, "labels": value_texts})
+        st.write('Race')
 
-            base = alt.Chart(chart_df).encode(
-                theta=alt.Theta(field="value", type="quantitative"),
-                color=alt.Color(field="race", type="nominal"),
-            )
+        categories = ['white','black','native_american','asian','hawaiian',	'some_other_race_alone','two_more_races','hispanic_or_latino']
 
-            pie = base.mark_arc(outerRadius=120)
-            text = base.mark_text(radius=140, size=20,color='black').encode(text="labels:N")
+        values = []
 
-            c = pie + text
+        value_texts = []
 
-            st.altair_chart(c)
+        for c in categories:
+            value = county_only_df[c].values[0]
+            values.append(value)
+            value_texts.append('{:.0%}'.format(value))
 
-            domain_cols_corr = {
-                "Financial wellbeing": ["median_family_income", "income_20_percentile", "income_80_percentile"],
-                "Education": ['preschool_enroll','avg_edu_prof_diff'],
-                
+        chart_df = pd.DataFrame({"race": categories, "value": values, "labels": value_texts})
+
+        base = alt.Chart(chart_df).encode(
+            theta=alt.Theta(field="value", type="quantitative"),
+            color=alt.Color(field="race", type="nominal"),
+        )
+
+        pie = base.mark_arc(outerRadius=120)
+        text = base.mark_text(radius=140, size=20,color='black').encode(text="labels:N")
+
+        c = pie + text
+
+        st.altair_chart(c)
+
+        domain_cols_corr = {
+            "Financial wellbeing": ["median_family_income", "income_20_percentile", "income_80_percentile"],
+            "Education": ['preschool_enroll','avg_edu_prof_diff'],
+            
                 "Neighborhoods":['proportion_high_poverty_neighborhood','transit_trips_index','transit_low_cost_index'],
                 "Housing": ["HOM_STUDENTS","proportion_homeless"],
                 
@@ -62,86 +102,82 @@ if fips_parameter in url_params:
             }
 
            
-            col1, col2 = st.columns(2)
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write(f'''
+                #### Financial Wellbeing         
+            ''')
+
+            get_metric("Median Family Income", "median_family_income", county_only_df, averages, '${0:,.0f}')
+            get_metric("Income 20%", "income_20_percentile",  county_only_df, averages, '${0:,.0f}')
+            get_metric("Income 80%", "income_80_percentile",  county_only_df, averages, '${0:,.0f}')
             
-            with col1:
-                st.write(f'''
-                    #### Financial Wellbeing         
-                ''')
+        with col2:
+            st.write(f'''
+                #### Education    
+            ''')
 
-                st.metric("Median Family Income",
-                        value = '${0:,.0f}'.format(county_only_df["median_family_income"].values[0]))
+            get_metric("Preschool enrollment","preschool_enroll", county_only_df, averages, '{:.0%}')
+            get_metric("Avg Edu Prof Diff", "avg_edu_prof_diff", county_only_df, averages)
 
-                st.metric("Income 20%",
-                        value = '${0:,.0f}'.format(county_only_df["income_20_percentile"].values[0]))
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write(f'''
+                #### Neighborhoods    
+            ''')
 
-                st.metric("Income 80%",
-                        value = '${0:,.0f}'.format(county_only_df["income_80_percentile"].values[0]))
-            with col2:
-                st.write(f'''
-                    #### Education    
-                ''')
-
-                st.metric("Preschool enrollment",
-                        value = '{:.0%}'.format(county_only_df["preschool_enroll"].values[0]))
-
-                st.metric("Avg Edu Prof Diff",
-                        value = county_only_df["avg_edu_prof_diff"].values[0])
-
-            col1, col2 = st.columns(2)
+            get_metric("Proportion High Poverty","proportion_high_poverty_neighborhood", county_only_df, averages, '{:.0%}', 'inverse')
+            get_metric("Transit Trips", "transit_trips_index", county_only_df, averages)
+            get_metric("Low Cost Transit", "transit_low_cost_index", county_only_df, averages)
+       
+        with col2:
+            st.write(f'''
+                #### Housing    
+            ''')
             
-            with col1:
-                st.write(f'''
-                    #### Neighborhoods    
-                ''')
-
-                st.metric("Proportion High Poverty",
-                        value = '{:.0%}'.format(county_only_df["proportion_high_poverty_neighborhood"].values[0]))
-
-                st.metric("Transit Trips",
-                        value = county_only_df["transit_trips_index"].values[0])
-
-                st.metric("Low Cost Transit",
-                        value = county_only_df["transit_low_cost_index"].values[0])
-
-            with col2:
-                st.write(f'''
-                    #### Housing    
-                ''')
-
-                st.metric("Homeless Students",
-                        value = county_only_df["HOM_STUDENTS"].values[0])
-
-                st.metric("Proportion homeless",
-                        value = '{:.0%}'.format(county_only_df["proportion_homeless"].values[0]))
+            get_metric("Homeless Students","HOM_STUDENTS", county_only_df, averages, 'inverse')
+            get_metric("Proportion homeless","proportion_homeless", county_only_df, averages, '{:.0%}', 'inverse')
             
-            col1, col2 = st.columns(2)
-            
-            with col1:
-                st.write(f'''
-                    #### Health    
-                ''')
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.write(f'''
+                #### Health    
+            ''')
 
-                st.metric("Low Birth Rate",
-                        value = '{:.0%}'.format(county_only_df["low_birth_rate"].values[0]))
+            st.metric("Low Birth Rate",
+                    value = '{:.0%}'.format(county_only_df["low_birth_rate"].values[0]))
 
-                st.metric("HPSA Score",
-                        value = county_only_df["HPSA Score"].values[0])
+            st.metric("HPSA Score",
+                    value = county_only_df["HPSA Score"].values[0])
 
-            with col2:
-                st.write(f'''
-                    #### Crime    
-                ''')
+        with col2:
+            st.write(f'''
+                #### Crime    
+            ''')
 
-                st.metric("Violent Crime Rate",
-                        value = county_only_df["low_birth_rate"].values[0])
+            st.metric("Violent Crime Rate",
+                    value = county_only_df["low_birth_rate"].values[0])
 
-                # st.metric("HPSA Score",
-                #         value = county_only_df["HPSA Score"].values[0])
-        else:
-            st.title(f"Cant find any data for {fips_code}")
+################################
+# page start             
+################################
 
+url_params = st.experimental_get_query_params()
 
+fips_parameter = 'fips'
 
+fips_code = ''
+
+if fips_parameter in url_params:
+    fips_code = url_params[fips_parameter][0]
+    
+    if len(fips_code) > 0 and fips_code.isnumeric():
+       print(fips_code)
+       build_page_content(fips_code)
+    else:
+        st.title(f"Cant find any data for {fips_code}")
 else:
-    st.title("Hello, World")
+    st.title("Upward Mobility Explorer")
