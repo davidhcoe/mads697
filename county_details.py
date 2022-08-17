2def515b4cd7d17e2a457e7d5246579dbecdce4c
Elizabeth Giancola <elizabethgiancola@Elizabeths-MacBook-Air.local>
2022-08-17 07:34:54 -0400

    update source descriptions

--- county_details.py
+++ county_details.py
@@ -298,7 +298,7 @@ def show_county_details_page():
                     st.markdown(
                         "*People in Poverty*<br>"
                         "Description:  Share of county residents experiencing poverty that live in high-poverty neighborhoods (a neighborhood is defined as a census tract).  A high-poverty neighborhood is one in which over 40 percent of the residents are experiencing poverty.<br>"
-                        "Source: American Community Survey (ACS) 5-year data, 2019",
+                        "Source: American Community Survey (ACS) 5-year data, 2019<br>",
                         unsafe_allow_html=True,
                     )
 
@@ -317,17 +317,25 @@ def show_county_details_page():
 
                     st.markdown(
                         "*Transportation access*<br>"
-                        "Description: Transit Index<br>"
+                        "Metric: Transit trips index<br>"
+                        "Description: This metric reflects the number of public transit trips taken annually by a three-person single-parent family with income at 50 percent of the Area Median Income for renters. This number is percentile ranked nationally into an index with values ranging from 0 to 100. Higher scores reflect better access to public transportation.<br>"
                         'Source: Department of Housing and Urban Development Accessed via <a href="https://hudgis-hud.opendata.arcgis.com/datasets/location-affordability-index-v-3/api">API</a><br>'
-                        "Notes: Converted the transit trips index (hh6_transit_trips_renters) given to percentile ranked nationally "
-                        "and inverted the transity cost index (hh6_t_renters) then converted to a percentile ranked nationally.<br>"
-                        "Interpretation: Higher scores reflect better access to public transportation.",
+                        "Assumptions: Converted the index (hh6_transit_trips_renters)  given to percentile ranked nationally.<br>"
+                        "<br>"
+                        
+                        "Metric: Low transportation cost index<br>"
+                        "Description: This index reflects local transportation costs as a share of renters’ incomes. It accounts for both transit and cars. This index is based on estimates of transportation costs for a three-person, single-parent family with income at 50 percent of the median income for renters for the county. Values are inverted and percentile ranked nationally, with values ranging from 0 to 100. The higher the value, the lower the cost of transportation in that neighborhood.<br>"
+                        'Source: Department of Housing and Urban Development Accessed via <a href="https://hudgis-hud.opendata.arcgis.com/datasets/location-affordability-index-v-3/api">API</a><br>'
+                        "Assumptions:  Inverted the original metric (hh6_t_renters) then converted to a percentile ranked nationally.<br>"
+                        ,
                         unsafe_allow_html=True,
                     )
                     st.markdown(
-                        '*Air Quality Index*<br>'
-                        "Description: AQI is an index used to report the daily air quality. The yearly median value is used here.<br>"
-                        'Source: <a href="https://aqs.epa.gov/aqsweb/airdata/download_files.html">Environmental Protection Agengy</a>',
+                        '*Environmental Quality*<br>'
+                        "Metric: Air quality index"
+                        "Description: The Air Quality Index, AQI, is an index used to report the daily air quality. This is used to indicate how clear or polluted the air is. This could be used as an indicator for various health effects that might be a concern in the area. The AQI is based on a score from 0 to 500 where zero is the cleanest and 500 is the most polluted.<br>"
+                        'Source: <a href="https://aqs.epa.gov/aqsweb/airdata/download_files.html">Environmental Protection Agengy</a><br>'
+                        "Notes: Median AQI from 2019 dataset was used.<br>",
                         unsafe_allow_html=True,
                     )
 
@@ -346,13 +354,14 @@ def show_county_details_page():
 
                 with st.expander("Source details"):
                     st.markdown(
-                        "Description: Share of the voting eligible population who turn out to vote<br>"
+                        "Metric: Share of the voting eligible population who turn out to vote<br>"
+                        "Description: This measures the share of the voting-eligible population who voted in the 2020 presidential election.<br>"
                         '''Source: MIT Election Data and Science Lab for 2020 Election pulled from the 
                         <a href="https://dataverse.harvard.edu/file.xhtml?fileId=6100388&version=1.1">Harvard Dataverse</a>
                         and American Community Survey 2020 5-year data<br>
                         MIT Election Data and Science Lab, 2022, "U.S. President Precinct-Level Returns 2020",
                         <a href="https://doi.org/10.7910/DVN/JXPREB">https://doi.org/10.7910/DVN/JXPREB</a>, Harvard Dataverse, V1<br>'''
-                        "Note: This measures the share of the voting-eligible population who voted in the 2020 presidential election.",
+                        ,
                         unsafe_allow_html=True,
                     )
 
@@ -388,14 +397,16 @@ def show_county_details_page():
                 with st.expander("Source details"):
                     st.markdown(
                         "*Exposure to crime*<br>"
-                        "Description: Rates of reported violent crime<br>"
+                        "Metric: Rates of reported violent crime<br>"
+                        "Description:  Violent crime is composed of four offenses: murder and nonnegligent manslaughter, rape, robbery, and aggravated assault. Rates are calculated as the number of reported crimes per 100,000 people. The FBI cautions using UCR data to rank or compare locales because this can create, ‘misleading perceptions which adversely affect geographic entities and their residents'.<br>"
                         "Source: Federal Bureau of Investigations (FBI) Uniform Crime Statistic (UCR) Crime in the United States, 2019, accessed via <a href='http://api.usa.gov/'>api.usa.gov</a>; American Community Survey 5-year data, 2019<br>"
-                        "Notes:  Violent crime is composed of four offenses: murder and nonnegligent manslaughter, rape, robbery, and aggravated assault. Rates are calculated as the number of reported crimes per 100,000 people. The FBI cautions using UCR data to rank or compare locales because this can create, ‘misleading perceptions which adversely affect geographic entities and their residents.’<br>"
+
                         "<br>"
                         "*Overly punitive policing*<br>"
-                        "Description: Juvenile arrests for each County.<br>"
-                        "Source:Easy Access to State and County Juvenile Court Case Counts (EZACO) available from <a href='https://www.ojjdp.gov/ojstatbb/ezaco/'> https://www.ojjdp.gov/ojstatbb/ezaco/ </a>.<br>"
-                         "Notes: ACS population data was used to calculate the rate of crime per 100k residents of each county. This metric includes both delinquency and status offenses. Delinquency referrals are made when youth violate criminal law. Status offenses are acts that are only illegal for youth under 18 (ex. truancy, ungovernability, liquor law violation, tobacco violation). High rates of juvenile arrests provide strong indicator of system involvement and over policing.<br>" 
+                        "Metric: Juvenile arrests for each County.<br>"
+                        "Description: This metric includes both delinquency and status offenses. Delinquency referrals are made when youth violate criminal law. Status offenses are acts that are only illegal for youth under 18 (ex. Runaway, truancy, ungovernability, liquor law violation, tobacco violation). High rates of juvenile arrests provide strong indicator of system involvement and over policing.<br>"
+                        "Source: Easy Access to State and County Juvenile Court Case Counts (EZACO) available from <a href='https://www.ojjdp.gov/ojstatbb/ezaco/'> https://www.ojjdp.gov/ojstatbb/ezaco/ </a>.<br>"
+                         "Notes: To calculate the rate of crime per 100k residents the ACS population data was used for each county. Juvenile arrests for each County. The 2019 data was used where available, but 2018 data was necessary for some states. The year of the data is noted by the states below. Not all states provide their data. States are noted if they did not have data in the last five years.<br>" 
                        ,
                         unsafe_allow_html=True,
                     )
@@ -494,13 +505,15 @@ def show_county_details_page():
                 with st.expander("Source details"):
                     st.markdown(
                         '*Income*<br>'
-                        "Description: Household income at 20th, 50th, and 80th percentiles<br>"
+                        "Metric: Household income at 20th, 50th, and 80th percentiles<br>"
+                        "Description: This set of measures reflects financial resources available to low, middle, and high-income households as well as the extent of income inequality.<br>"
                         "Source: ACS 5-year data, 2019",
                         unsafe_allow_html=True,
                     )
                     st.markdown(
                         '*Financial security*<br>'
-                        "Description: Share of Share of households with debt in collections<br>"
+                        "Metric: Share of households with debt in collections<br>"
+                        "Description: The measure accounts for the share of households in an area with debt that has progressed from being past due to being in collections.<br>"
                         '''Source: Alexander Carther, Kassandra Martinchek, Breno Braga, Signe-Mary McKernan, and Caleb Quakenbush. 2021. 
                         Debt in America 2022. Accessible from <a href="https://datacatalog.urban.org/dataset/debt-america-2022">https://datacatalog.urban.org/dataset/debt-america-2022</a>''',
                         unsafe_allow_html=True,
@@ -521,8 +534,9 @@ def show_county_details_page():
 
                 with st.expander("Source details"):
                     st.markdown(
-                        "Description: Portion of public-school children who had ever experienced housing instability in the 2018-2019 school year<br>"
-                        'Source: <a href="https://www2.ed.gov/about/inits/ed/edfacts/data-files/school-status-data.html">US Department of Education</a>',
+                        "Metric: Portion of public-school children who had ever experienced housing instability in the 2018-2019 school year<br>"
+                        "Description: The number homeless is based on the number of children (age 3 through 12th grade) who are enrolled in public schools and whose primary nighttime residence at any time during a school year was a shelter, transitional housing, or awaiting foster care placement; unsheltered (e.g., a car, park, campground, temporary trailer, or abandoned building); a hotel or motel because of the lack of alternative adequate accommodations; or in housing of other people because of loss of housing, economic hardship, or a similar reason. The share is the percent of all public school students in those schools experiencing homelessness.<br>"
+                        'Source: <a href="https://www2.ed.gov/about/inits/ed/edfacts/data-files/school-status-data.html">US Department of Education</a> via EdFacts Homeless Students Enrolled 2018-2019 School Year',
                         unsafe_allow_html=True,
                     )
 
@@ -603,16 +617,20 @@ def show_county_details_page():
 
                 with st.expander("Source details"):
                     st.markdown(
-                        "*Health Professional Shortage Area ranking for primary care providers*<br>"
+                        "*Access to and utilization of health services*<br>"
+                        "Metric: Health Professional Shortage Area ranking for primary care providers<br>"
+                        "Description: The Health Resources & Services Administration designates an area as having a shortage of Primary Care providers for an entire group of people within a defined geographic area and indicates the severity of the shortage with the HPSA Score using values from 0 to 25, where higher scores indicate a greater shortage.<br>"
                         'Source: <a href="https://data.hrsa.gov/data/download">Health Resources & Services Administration</a><br>'
-                        "Note: This attribute represents the Health Professional Shortage Area Score developed by the National Health Service Corps in determining priorities for assignment of clinicians. The scores range from 0 to 26 where the higher the score, the greater the priority.<br>",
+                        "Note: 'Designated' and 'Proposed For Withdrawal' were used to indicate currently having a health shortage for the category of 'Geographical' shortage for each county.<br>",
                         unsafe_allow_html=True,
                     )
 
                     st.markdown(
-                        "*Share of low-weight births*<br>"
-                        'Source: <a href="https://wonder.cdc.gov/natality-expanded-current.html">Center for Disease Control </a><br>'
-                        "Notes: The proportion of babies born weighing less than 5 pounds 8 ounces (<2,500 grams) out of all births.<br>",
+                        "*Neonatal Health*<br>"
+                        "Metric: Share of low-weight births<br>"
+                        "Notes: The proportion of babies born weighing less than 5 pounds 8 ounces (<2,500 grams) out of all births.<br>"
+                        'Source: National Vital Statistics System, Natality on <a href="https://wonder.cdc.gov/natality-expanded-current.html">CDC WONDER</a>, Natality public use data 2019 <br>'
+                        ,
                         unsafe_allow_html=True,
                     )
 
@@ -698,11 +716,13 @@ def show_county_details_page():
 
                     st.markdown(
                         "*Access to preschool*<br>"
-                        "Description: Share of children enrolled in nursery school or preschool<br>"
+                        "Metric: Share of children enrolled in nursery school or preschool<br>"
+                        "Description: This metric measures the share of the county's three to four year old children who are enrolled in nursery or preschool.<br>"
                         "Source: ACS 5-year data, 2019<br><br>"
-                        """ *Effective Education*<br>Description: Average per-grade change in English Language Arts Achievement, between third and eighth grades<br>
+                        """ *Effective Education*<br>Metric: Average per-grade change in English Language Arts Achievement, between third and eighth grades<br>
+                        Description: This metric reports the average annual improvement in English (reading comprehension, written expression) observed between the third and eighth grades for each jurisdiction.<br>
                         Source: <a href="https://www2.ed.gov/about/inits/ed/edfacts/data-files/index.html">US Department of Education</a>, 2018-2019 school year<br>
-                        Notes: Calculated using weighted average for differences within the same county. <br><br>
+                        Notes: Scores in the original dataset that are ranges less than 10, the median value was used. Ranges larger than 10 were not used. If there are multiple averages, the final value was calculated using weighted average for differences within the same county.<br><br>
                         """,
                         unsafe_allow_html=True,
                     )
@@ -778,9 +798,9 @@ def show_county_details_page():
                 with st.expander("Source details"):
                     st.markdown(
                         """*Employment*<br>
-                            Description: Employment-to-population ratio for adults<br>
+                            Metric: Employment-to-population ratio for adults<br>
+                            Description: This metric is the ratio of the number of employed adults in the county to the total number of adults in that age range living there.<br>
                             Source: ACS 5-year data, 2019<br>
-                            Notes: Calculated using civilian population
                             """,
                         unsafe_allow_html=True,
                     )
